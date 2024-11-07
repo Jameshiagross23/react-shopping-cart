353371bb5e03a7f8f110a62d69b6a53a945c88d1
Jameshia R Gross <143536921+Jameshiagross23@users.noreply.github.com>
2024-11-07 08:59:20 -0600

    lab 05-01 turn in

--- shopping_cart/src/App.jsx
+++ shopping_cart/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import CartList from './CartList'
 function App() {
 
--- shopping_cart/src/CartList.jsx
+++ shopping_cart/src/CartList.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
+
 export default function CartList(){
 
 
@@ -22,33 +23,36 @@ export default function CartList(){
     setItems(newItems);
   }
   function onAddQuantity(item){
-    const newQuantity = item.quantity = 1;
-    if(newQuantity < 10){
+    
+    const newQuantity = item.quantity + 1;
+    if(newQuantity <= 10){
       const newItems = [...items];
       const index = items.indexOf(item);
       newItems[index].quantity++;
       setItems(newItems);
     }
-    const newItems = [...items];
-    const index = items.indexOf(item);
-    newItems[index].quantity++;
-    setItems(newItems);
   }
-  function onDeleteItem(item){
-    const deletedItems = [...items];
-    const index = items.indexOf(item);
-    deletedItems[index].quantity--;
-     setItems(deletedItems); 
-    
+  function onDeleteItem(item) {
+    const newItemQuantity = item.quantity - 1;
+  
+    if (newItemQuantity >= 0) {
+      const updatedItems = items.map(i => 
+        i.id === item.id ? { ...i, quantity: i.quantity - 1 } : i
+      );
+      setItems(updatedItems);
+    } else {
+      const updatedItems = items.filter(i => i.id !== item.id);
+      setItems(updatedItems);
+    }
   }
   return(
     <> 
-    <div className="container">
-      <span className='fs-1 text-primary me-3'>Shopping Cart</span>
-      <span className="fs-3 mb-4 badge rounded-circle text-bg-primary">{itemCount}</span>
+    <div className="contain">
+      <span className='fs-1 text-primary me-3'>Christmas Party Shopping Cart</span>
+      <span className="fs-3 mb-4 badge rounded-circle text-bg-primary">{itemCount > 0 ? itemCount : 'Add Items To Cart' }</span>
       <div>
 
-         <button type="button" className="mb-4 btn btn-primary btn-sm" onClick={() => setItems([...items, { id: nanoid(), name: '', quantity: 1 }])}>Add Item</button>
+         <button type="button" className="mb-4 btn btn-primary btn-sm" onClick={() => setItems([...items, { id: nanoid(), name: '', quantity: 1 }])}><p>Add &quot;Name : Item&quot;</p><p>Or Click Here To Add More Items To the List!</p></button>
       </div>  
      
    {items.map(item => 
@@ -56,12 +60,13 @@ export default function CartList(){
       <div className='col-4 mb-4'>
      <input type='text' className={item.name.length > 0 ? 'form-control is-valid': 'form-control is-invalid'} value={item.name} onChange={(evt)=> onNameChange(evt,item)} />
         </div>
-        <div className='col-4'>
+        <div className='col-4 bg-dark text-light'>
           <span>{item.quantity}</span>  
           </div>
         <div className='col-4'>
-          <button className='btn btn-primary rounded-circle me-3' onClick={() => onAddQuantity(item)}>+</button>
-          <button className='btn btn-danger rounded-circle' onClick={() =>  onDeleteItem(item)}>-</button>
+          <button className='btn btn-primary rounded-circle me-3' disabled={item.quantity == 10 ?  true : false} onClick={() => onAddQuantity(item)}>+</button>
+          <button className='btn btn-danger rounded-circle' disabled={item.quantity == 0 ?  true : false} onClick={() =>  onDeleteItem(item)}>-</button>
+          <button className='btn btn-danger rounded-circle' hidden={item.quantity > 0 ?  true : false} onClick={() =>  onDeleteItem(item)}>Delete From Shopping Cart</button>
         
       </div>
       </div>
